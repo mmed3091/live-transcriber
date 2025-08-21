de3ec6c9054dd51bb1236dae5cbe755a4e1d3740
mmed <mila.medic2000@gmail.com>
2025-08-21 11:51:11 +0200

    created basic mic button component

---
+++ language-chatbot/src/components/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header () {
+
+    return <h1>Welcome to my Language Learning Chatbot! :P </h1>
+
+}
---
+++ language-chatbot/src/components/MicButton.tsx
@@ -0,0 +1,8 @@
+import "../index.css";
+
+
+function MicButton() {
+  return <button className="mic-button">🎤 </button>;
+}
+
+export default MicButton;
