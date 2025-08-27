7a8f724e9450dd10c84788fdfeeff6f1054f6029
mmed <mila.medic2000@gmail.com>
2025-08-27 16:15:25 +0200

    multi-language transcription enabled

--- language-chatbot/src/App.tsx
+++ language-chatbot/src/App.tsx
@@ -1,86 +1,82 @@
-import { useState } from 'react'
-import { useRef } from 'react'
-import './App.css'
+import { useState } from "react";
+import { useRef } from "react";
+import "./App.css";
 
 function App() {
-
   const apiKey = import.meta.env.VITE_API_KEY;
-  const [transcript, setTranscript] = useState('');
+  const [transcript, setTranscript] = useState("");
   const [isTranscribing, setIsTranscribing] = useState(false);
   const socketRef = useRef<WebSocket | null>(null);
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
   const streamRef = useRef<MediaStream | null>(null);
 
   const handleTranscriptionToggle = async () => {
-    if(isTranscribing) {
-
+    if (isTranscribing) {
       mediaRecorderRef.current?.stop();
       streamRef.current?.getTracks().forEach((track) => track.stop()); // close all audio/media streams
       socketRef.current?.close();
       setIsTranscribing(false);
-    } 
-    else {
+    } else {
       try {
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true}); // API to ask permission for microphone use 
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+        }); // API to ask permission for microphone use
         streamRef.current = stream;
 
-        const mediaRecorder = new MediaRecorder(stream,  {mimeType: 'audio/webm'}) // API fo recording media streams - record audio in webm format with audio only WebM file output
+        const mediaRecorder = new MediaRecorder(stream, {
+          mimeType: "audio/webm",
+        }); // API fo recording media streams - record audio in webm format with audio only WebM file output
         mediaRecorderRef.current = mediaRecorder;
 
-        const socket = new WebSocket('wss://api.deepgram.com/v1/listen?model=nova-3', // create a new Websocket conncetion to the server at the URL
-          ['token', // subprotocols 
-          apiKey]
-        )
+        const socket = new WebSocket(
+          "wss://api.deepgram.com/v1/listen?language=multi&model=nova-3", // create a new Websocket conncetion to the server at the URL
+          [
+            "token", // subprotocols
+            apiKey,
+          ]
+        );
         socketRef.current = socket;
 
-
         socket.onopen = () => {
-          mediaRecorder.addEventListener('dataavailable', (event) =>{ 
-            if (socket.readyState === WebSocket.OPEN) { // check if WebSocket still open once data has been received
+          mediaRecorder.addEventListener("dataavailable", (event) => {
+            if (socket.readyState === WebSocket.OPEN) {
+              // check if WebSocket still open once data has been received
               socket.send(event.data); // send recorder audio to Deepgram
             }
           });
-          mediaRecorder.start(250); 
-        }
+          mediaRecorder.start(250);
+        };
 
-        socket.onmessage = (message) => { // WebSocket receives a message from the server
+        socket.onmessage = (message) => {
+          // WebSocket receives a message from the server
           const received = JSON.parse(message.data);
           const result = received.channel.alternatives[0]?.transcript; // get the transcript with the highest confidence index
-          if(result) {
-            setTranscript((prev) => prev + ' ' + result);
+          if (result) {
+            setTranscript((prev) => prev + " " + result);
           }
         };
 
         setIsTranscribing(true);
-
-
       } catch (err) {
-        console.error('Failed to start transcription', err);
-
+        console.error("Failed to start transcription", err);
       }
-      
     }
-  }
-  
-
-
-
+  };
 
   return (
     <div className="app">
-      <h1 className='header'>Welcome to my Language Learning Chatbot!</h1>
+      <h1 className="header">Welcome to my Language Learning Chatbot!</h1>
 
-      <button onClick={handleTranscriptionToggle} className='toggle-button'>
-        {isTranscribing ? 'Stop Transcription' : 'Begin Transcription'}
-
+      <button onClick={handleTranscriptionToggle} className="toggle-button">
+        {isTranscribing ? "Stop Transcription" : "Begin Transcription"}
       </button>
 
-      <div className='transcript-box'>
-        {transcript || (isTranscribing ? 'Listening...' : 'Click the button to begin')}
+      <div className="transcript-box">
+        {transcript ||
+          (isTranscribing ? "Listening..." : "Click the button to begin")}
       </div>
-
     </div>
   );
 }
 
-export default App
+export default App;
